ccacde02cde79e52be283ffec9d2f6347908b7c2
pralr <101532384+pralr@users.noreply.github.com>
2022-03-31 22:08:00 -0300

    alocação dinâmica

---
+++ realloc.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int max, newMax, *ptr; 
+
+    printf("Escolha o numero maximo para o array: \n");
+    scanf("%d", &max); 
+
+    ptr = (int*)malloc(max*(sizeof(int))); 
+
+    if(ptr!=NULL) {
+    
+        printf("Insira os valores: \n");
+        for(int i = 0; i<max; i++) 
+            scanf("%d", ptr+i); 
+        
+        for(int k = 0; k<max; k++) 
+            printf("%d ", *(ptr+k)); // acessando o conteúdo do ponteiro *
+        printf("foram adicionados.\n");
+
+        // agora vamos realocar memória
+        // lembre que os parametros são nosso ponteiro e o novo tam
+
+        printf("Insira um novo valor para o maximo: \n"); 
+        scanf("%d", &newMax); 
+        ptr = (int*)realloc(ptr, newMax*sizeof(int)); 
+
+        // insira os novos elementos
+        // ao invés de j = 0 para inserir, você coloca j = max, pois você vai inserir depois dos valores já existentes
+        printf("Insira %d elementos: \n", newMax-max);
+        for(int j = max; j<newMax; j++) {
+            scanf("%d", ptr+j);
+        }
+
+        printf("novo Array: "); 
+        for(int m = 0; m<newMax; m++) {
+            printf("%d ", *(ptr+m)); 
+        }        
+
+    } else {
+        printf("Memoria nao alocada.\n");
+    }
+
+    free(ptr);
+    return 0;
+}
